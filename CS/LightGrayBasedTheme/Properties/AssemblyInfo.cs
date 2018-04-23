77ba46067fa21d20e2bbf7970c403ad32287c136
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LightGrayBasedTheme/Properties/AssemblyInfo.cs
@@ -0,0 +1,76 @@
+﻿// Developer Express Code Central Example:
+// How to apply a custom theme to grid elements
+// 
+// The following example demonstrates how to create a custom theme and apply it to
+// DXGrid control elements.
+// In this example the custom theme assembly is used to
+// customize grid control elements.
+// Please notice that this assembly should follow
+// this naming rule: DevExpress.Xpf.Themes.CustomAssemblyName.VersionNumber
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E2882
+
+#region Copyright (c) 2000-2011 Developer Express Inc.
+/*
+{*******************************************************************}
+{                                                                   }
+{       Developer Express .NET Component Library                    }
+{                                                                   }
+{                                                                   }
+{       Copyright (c) 2000-2011 Developer Express Inc.              }
+{       ALL RIGHTS RESERVED                                         }
+{                                                                   }
+{   The entire contents of this file is protected by U.S. and       }
+{   International Copyright Laws. Unauthorized reproduction,        }
+{   reverse-engineering, and distribution of all or any portion of  }
+{   the code contained in this file is strictly prohibited and may  }
+{   result in severe civil and criminal penalties and will be       }
+{   prosecuted to the maximum extent possible under the law.        }
+{                                                                   }
+{   RESTRICTIONS                                                    }
+{                                                                   }
+{   THIS SOURCE CODE AND ALL RESULTING INTERMEDIATE FILES           }
+{   ARE CONFIDENTIAL AND PROPRIETARY TRADE                          }
+{   SECRETS OF DEVELOPER EXPRESS INC. THE REGISTERED DEVELOPER IS   }
+{   LICENSED TO DISTRIBUTE THE PRODUCT AND ALL ACCOMPANYING .NET    }
+{   CONTROLS AS PART OF AN EXECUTABLE PROGRAM ONLY.                 }
+{                                                                   }
+{   THE SOURCE CODE CONTAINED WITHIN THIS FILE AND ALL RELATED      }
+{   FILES OR ANY PORTION OF ITS CONTENTS SHALL AT NO TIME BE        }
+{   COPIED, TRANSFERRED, SOLD, DISTRIBUTED, OR OTHERWISE MADE       }
+{   AVAILABLE TO OTHER INDIVIDUALS WITHOUT EXPRESS WRITTEN CONSENT  }
+{   AND PERMISSION FROM DEVELOPER EXPRESS INC.                      }
+{                                                                   }
+{   CONSULT THE END USER LICENSE AGREEMENT FOR INFORMATION ON       }
+{   ADDITIONAL RESTRICTIONS.                                        }
+{                                                                   }
+{*******************************************************************}
+*/
+#endregion Copyright (c) 2000-2011 Developer Express Inc.
+
+using System.Windows.Markup;
+using System.Resources;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using DevExpress.Internal;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("Xpf.Core Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Developer Express Inc.")]
+[assembly: AssemblyProduct("DevExpress.Xpf.Themes.LightGray.Theme")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2011 Developer Express Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+[assembly: SatelliteContractVersion(AssemblyInfo.SatelliteContractVersion)]
+[assembly: ComVisible(false)]
+[assembly: Guid("4E99E381-22BA-4A62-8F3C-0292A924FBEC")]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.FileVersion)]
+#pragma warning disable 1699
+
+#pragma warning restore 1699
